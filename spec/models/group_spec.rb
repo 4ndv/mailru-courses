0602dd43e1bf170bfb060d5b5e83c4abb1996295
Andrey Viktorov <andreyviktorov@outlook.com>
2020-07-26 16:16:11 +0700

    Group spec

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Group, type: :model do
   subject { create(:group) }
 
   it { should be_kind_of(Group) }
+
+  describe "start_at" do
+    it { should validate_presence_of(:start_at) }
+  end
 end
