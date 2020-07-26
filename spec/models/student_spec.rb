e1306ba74db3980118d62602bae54f83dbf992cf
Andrey Viktorov <andreyviktorov@outlook.com>
2020-07-26 16:19:54 +0700

    Student spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -4,4 +4,20 @@ RSpec.describe Student, type: :model do
   subject { create(:student) }
 
   it { should be_kind_of(Student) }
+
+  describe "name" do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(255) }
+  end
+
+  describe "email" do
+    it { should validate_presence_of(:email) }
+    it { should validate_length_of(:email).is_at_least(1).is_at_most(255) }
+
+    it { should allow_value("test@email.com").for(:email) }
+    it { should allow_value("also-correct@email").for(:email) }
+    it { should_not allow_value("!@#$%^&*(@email").for(:email) }
+
+    it { should validate_uniqueness_of(:email).scoped_to(:group_id) }
+  end
 end
