f620b64032047c3f7b0ab62998bfbd50c2db69cc
Andrey Viktorov <andreyviktorov@outlook.com>
2020-07-26 20:24:20 +0700

    ActiveAdmin with theme and addons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root to: "courses#index"
 
   get "/courses/:id", to: "courses#show", as: :course
