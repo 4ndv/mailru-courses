68b872e25f62520bda37d4be5e792399c5b2ca1e
Andrey Viktorov <andreyviktorov@outlook.com>
2020-07-27 15:08:43 +0700

    Dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,31 +4,23 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Registered students count" do
+          table_for Course.where.not(students_count: 0).order(title: :asc) do
+            column("Course") { |course| link_to(course.title, admin_students_path(q: { course_id_eq: course.id })) }
+            column("Students") { |course| course.students_count }
+          end
+        end
+      end
+
+      column do
+        panel "Courses without students" do
+          table_for Course.where(students_count: 0).order(title: :asc) do
+            column("Course") { |course| link_to(course.title, admin_students_path(q: { course_id_eq: course.id })) }
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+  end
 end
