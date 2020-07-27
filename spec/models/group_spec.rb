6a43ed9741f860b02447295d7ca507c3c5d01fb0
Andrey Viktorov <andreyviktorov@outlook.com>
2020-07-27 15:11:34 +0700

    Test Group#update_closest_starting_group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Group, type: :model do
   describe "start_at" do
     it { should validate_presence_of(:start_at) }
   end
+
+  describe "#update_closest_starting_group" do
+    it "must update it's course if it starts before" do
+      group = create(:group)
+
+      expect(group.course.closest_starting_group.id).to eq(group.id)
+
+      new_group = create(:group, course: group.course, start_at: group.start_at - 2.days)
+
+      expect(group.course.closest_starting_group.id).to eq(new_group.id)
+    end
+  end
 end
